a3950f73603a1cb3cf799165fddd0b3f985d696b
Jair Carbajal <jair.carbajal91@gmail.com>
2022-08-22 10:59:07 -0700

    products reducer set up

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Product
 
-product_routes = Blueprint('products',__name__)
+product_routes = Blueprint('products', __name__, url_prefix='/api/products')
 
 @product_routes.route('/')
 def all_products():
