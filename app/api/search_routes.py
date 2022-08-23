0e25d77dad1017f6c0d97dfd1781c2992a716dc2
alice886 <baconhill4tmr@gmail.com>
2022-08-23 16:56:01 -0700

    search filters finished

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -4,12 +4,13 @@ from app.models import db, Product, User, Cart
 from app.forms import ListForm, CartItemForm
 from datetime import datetime, date, timedelta
 from .auth_routes import validation_errors_to_error_messages
+from sqlalchemy import or_
 
 search_routes = Blueprint('search', __name__)
 
 def get_filter(key, value):
     if key == 'keyword':
-        return [Product.name.like(f'%{value}%'),Product.description.like(f'%{value}%')]
+        return [or_(Product.name.like(f'%{value}%'), Product.description.like(f'%{value}%'))]
     elif key == 'minPrice':
         return [Product.price >= value]
     elif key == 'maxPrice':
@@ -24,5 +25,13 @@ def search():
     filters = []
     args = request.args
     args_dict = args.to_dict()
-    filtered_products = Product.query.filter()
-    return args_dict
+    print(args_dict)
+    for k,v in args_dict.items():
+        f = get_filter(k,v)
+        if f is not False:
+            filters.extend(f)
+    if len(filters) > 0:
+        filtered_products = Product.query.filter(*filters).all()
+    else: 
+        filtered_products = Product.query.all()
+    return {'products': [product.to_dict() for product in filtered_products]}
