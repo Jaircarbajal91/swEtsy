7b0ea22339da3d89fc885011e279b126645eaf60
kevin <zhang.kevin.hua@gmail.com>
2022-08-23 18:59:10 -0500

    get cart item action thunk and reducer initialized

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -7,5 +7,25 @@ const getCartItemsAction = cartItems => ({
 });
 
 export const getCartItemsThunk = () => async dispatch => {
-    const response = await fetch('/api/')
-}
+    const response = await fetch('/api/cart');
+    if (response.ok) {
+        const cartItems = await response.json();
+        dispatch(getCartItemsAction(cartItems));
+        return cartItems;
+    };
+};
+
+export default function cartItemsReducer(state = {}, action) {
+    switch (action.type) {
+        case GET_CART_ITEMS: {
+            const newState = {};
+            action.cartItems.cart_details.forEach(item => {
+                newState[item.id] = item;
+            });
+            newState.cartItemsList = [ ...action.cartItems.cart_details ];
+            return newState;
+        };
+        default:
+            return state;
+    };
+};
