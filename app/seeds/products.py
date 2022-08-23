641ccbe508f563ffcff2272183caafb4291c16dc
zerotume <zerotume@gmail.com>
2022-08-22 21:10:56 -0700

    added seed file

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -21,5 +21,5 @@ def seed_products():
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
 def undo_products():
-    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE products RESTART IDENTITY CASCADE;')
     db.session.commit()
