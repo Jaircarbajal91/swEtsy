0df59e2c83407e306c1eb761c74fecc3b8a9d330
alice886 <baconhill4tmr@gmail.com>
2022-08-23 16:34:02 -0700

    finished search helper function

---
+++ app/api/search_routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required,current_user
+from app.models import db, Product, User, Cart
+from app.forms import ListForm, CartItemForm
+from datetime import datetime, date, timedelta
+from .auth_routes import validation_errors_to_error_messages
+
+search_routes = Blueprint('search', __name__)
+
+def get_filter(key, value):
+    if key == 'keyword':
+        return [Product.name.like(f'%{value}%'),Product.description.like(f'%{value}%')]
+    elif key == 'minPrice':
+        return [Product.price >= value]
+    elif key == 'maxPrice':
+        return [Product.price <= value]
+    elif key == 'ownerId':
+        return [Product.owner_id == value]
+    else:
+        return False
+
+@search_routes.route('/',methods=['GET'])
+def search():
+    filters = []
+    args = request.args
+    args_dict = args.to_dict()
+    filtered_products = Product.query.filter()
+    return args_dict
