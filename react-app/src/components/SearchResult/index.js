9594c30ee1b34eaafd4d79e7356a8da510b9cc14
alice886 <baconhill4tmr@gmail.com>
2022-08-24 10:59:42 -0700

    added sort

--- react-app/src/components/SearchResult/index.js
+++ react-app/src/components/SearchResult/index.js
@@ -7,37 +7,60 @@ const SearchResult = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const [inputText, setInputText] = useState('');
-    const [showModal, setShowModal] = useState(false)
+    const [showFilterModal, setShowFilterModal] = useState(false)
+    const [sortBy, setSortBy] = useState('')
 
     const sessionUser = useSelector(state => state.session.user);
     const products = useSelector(state => state.session.products);
+
+    const sortSelected = async e => {
+        e.preventDefault();
+        setSortBy(e.target.value);
+    }
+
     return (
         <>
-            <button onClick={() => setShowModal(true)}>All Filters</button>
-            {showModal &&
-                <Modal onClose={() => setShowModal(false)}>
-                    <h1>Filters</h1>
+            <button onClick={() => setShowFilterModal(true)}>All Filters</button>
+            {showFilterModal &&
+                <Modal onClose={() => setShowFilterModal(false)}>
                     <div>
-                        <div>Keyword Search
+                        <h1>Filters</h1>
+                        <fieldset> Keyword Search
                             <br></br>
-                            <input></input>
-                        </div>
+                            <input type='text'></input>
+                            <button>clear</button>
+                        </fieldset>
                         <br></br>
-                        <div>Price
-                            <br></br>
-                            <input type="radio" name='range 0 to 50' value={{ 'minPrice': 0, 'maxPrice': 50 }} />{`$0 to $500`} <br></br>
-                            <input type="radio" name='range 50 to 100' value={{ 'minPrice': 50, 'maxPrice': 100 }} />{`$50 to $100`} <br></br>
-                            <input type="radio" name='range 100 to 200' value={{ 'minPrice': 100, 'maxPrice': 200 }} />{`$100 to $200`} <br></br>
-                            <br></br>
-                            <div> Custom Price Range
-                                <div className='search price title'>Low</div>
-                                <input className='search price input' />
-                                <div className='search price title'>High</div>
-                                <input className='search price input' />
+                        <fieldset>Price
+                            <div>
+                                <input type="radio" name='price' value={{ 'minPrice': 0, 'maxPrice': 50 }} />{`$0 to $500`} <br></br>
                             </div>
-                        </div>
+                            <div>
+                                <input type="radio" name='price' value={{ 'minPrice': 50, 'maxPrice': 100 }} />{`$50 to $100`} <br></br>
+                            </div>
+                            <div>
+                                <input type="radio" name='price' value={{ 'minPrice': 100, 'maxPrice': 200 }} />{`$100 to $200`} <br></br>
+                            </div>
+                            <div>
+                                <input type="radio" name='price' />{`Custom Price Range`}<br></br>
+                            </div>
+                            <div>
+                                <input type="number" name='min' placeholder='Low' step={1} min={0} /><br></br>
+                                <input type="number" name='max' placeholder='High' step={1} min={0} /><br></br>
+                            </div>
+                        </fieldset>
+                        <br></br>
+                        <button>Cancel</button>
+                        <button>Apply</button>
                     </div>
                 </Modal>}
+            <select className='search sort' onChange={sortSelected} value={sortBy}>
+                <option value='' selected disabled> Sort by</option>
+                <option value='lowest'>Lowest Price</option>
+                <option value='highest'>Highest Price</option>
+                <option value='topreview'>Top Customer Reviews</option>
+                <option value='mostrecent'>Most Recent</option>
+            </select>
         </>
     )
 }
