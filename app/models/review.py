87d1a9fe847f68eaef8407d309a765d307bcefd2
kevin <zhang.kevin.hua@gmail.com>
2022-08-25 01:25:36 -0500

    review model rough draft fin

--- app/models/review.py
+++ app/models/review.py
@@ -8,4 +8,21 @@ class Review(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     stars = db.Column(db.Integer, nullable=False)
     review_body = db.Column(db.String(1000))
-    product_id = db.Column
+    product_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship('User', back_populates='user_reviews', foreign_keys=[user_id])
+    product = db.relationship('Product', back_populates='reviews', foreign_keys=[product_id])
+
+    @property
+    def review_details(self):
+        return self.to_dict()
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'stars': self.stars,
+            'review_body': self.review_body,
+            'product_id': self.product_id,
+            'user_id': self.user_id
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -17,6 +17,7 @@ class User(db.Model, UserMixin):
 
     products = db.relationship("Product", back_populates="user")
     carts = db.relationship("Cart", back_populates="user")
+    user_reviews = db.relationship('Review', back_populates='user')
 
     @property
     def password(self):
