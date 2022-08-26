62d3d1b5e09020c93522dc32ecfb39988d6b9656
zerotume <zerotume@gmail.com>
2022-08-25 21:01:36 -0700

    finished enter trigger search

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -40,13 +40,41 @@ const SearchBar = ({searchWords, setSearchWords}) => {
     }
   }
 
+  const handleSearchPress = e => {
+    if(e.keyCode === 13){
+      if(path.includes('search')){
+        let query = new URLSearchParams(loc.search)
+        let filtered = []
+        let data = {
+          keyword:searchWords,
+          minPrice:query.get('minPrice'),
+          maxPrice:query.get('maxPrice'),
+          ownerId:query.get('ownerId'),
+          order:query.get('order')
+        }
+        for (let key in data) {
+            if (data[key]) {
+                filtered.push(`${key}=${data[key]}`)
+            }
+        }
+      let filterstring = filtered.join("&")
+      history.push(`/search?${filterstring}`)
+      }else{
+        history.push(`/search?keyword=${searchWords}`)
+      }
+      e.target.blur()
+    }
+  }
+
   return (
     <div className="search-bar-container">
       <input className='input search-bar'
               type="text"
               placeholder='Search for anything'
               value={searchWords}
-              onChange={e => setSearchWords(e.target.value)}/>
+              onChange={e => setSearchWords(e.target.value)}
+              onKeyDown={handleSearchPress}
+              />
       <button className='input search-submit' onClick={searchClickHandler}>o-</button>
     </div>
   )
