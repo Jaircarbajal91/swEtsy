8718c565927c265b387dda465821f0018dd79601
Jair Carbajal <jair.carbajal91@gmail.com>
2022-08-26 21:24:30 -0700

    navbar styling completed

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
+import Magnifying from "../images/Magnifying.svg"
 import './SearchBar.css'
 
-const SearchBar = ({searchWords, setSearchWords}) => {
+const SearchBar = ({ searchWords, setSearchWords }) => {
   const [symbolErrors, setSymbolErrors] = useState('')
   const [disableSearch, setDisableSearch] = useState(false)
   const history = useHistory()
@@ -10,23 +11,23 @@ const SearchBar = ({searchWords, setSearchWords}) => {
   const path = loc.pathname
   // console.log(path)
   useEffect(() => {
-    if(path.includes('search')){
+    if (path.includes('search')) {
       let query = new URLSearchParams(loc.search)
       setSearchWords(query.get('keyword') || '')
-    }else{
+    } else {
       setSearchWords('')
     }
-  },[path])
+  }, [path])
 
   useEffect(() => {
-    if(!(/^[A-Za-z0-9 \-]*$/g.test(searchWords))){
-         setSymbolErrors('Only alphabates, numbers, spaces and - permitted for search!')
-         setDisableSearch('true')
-    }else{
+    if (!(/^[A-Za-z0-9 \-]*$/g.test(searchWords))) {
+      setSymbolErrors('Only alphabates, numbers, spaces and - permitted for search!')
+      setDisableSearch('true')
+    } else {
       setSymbolErrors('')
       setDisableSearch(false)
     }
-  },[searchWords])
+  }, [searchWords])
 
   const searchChangeHandler = e => {
     setSearchWords(e.target.value)
@@ -36,24 +37,24 @@ const SearchBar = ({searchWords, setSearchWords}) => {
     console.log(searchWords);
     setSymbolErrors('')
     setSearchWords(prev => prev.replaceAll(/[^A-Za-z0-9\- ]/g, ''))
-    if(path.includes('search')){
+    if (path.includes('search')) {
       let query = new URLSearchParams(loc.search)
       let filtered = []
       let data = {
-        keyword:searchWords,
-        minPrice:query.get('minPrice'),
-        maxPrice:query.get('maxPrice'),
-        ownerId:query.get('ownerId'),
-        order:query.get('order')
+        keyword: searchWords,
+        minPrice: query.get('minPrice'),
+        maxPrice: query.get('maxPrice'),
+        ownerId: query.get('ownerId'),
+        order: query.get('order')
       }
       for (let key in data) {
-          if (data[key]) {
-              filtered.push(`${key}=${data[key]}`)
-          }
+        if (data[key]) {
+          filtered.push(`${key}=${data[key]}`)
+        }
       }
-    let filterstring = filtered.join("&")
-    history.push(`/search?${filterstring}`)
-    }else{
+      let filterstring = filtered.join("&")
+      history.push(`/search?${filterstring}`)
+    } else {
       history.push(`/search?keyword=${searchWords}`)
     }
   }
@@ -65,27 +66,27 @@ const SearchBar = ({searchWords, setSearchWords}) => {
     //     return prev.replaceAll(/[^A-Za-z0-9 ]/g, '')
     //   })
     // }
-    if(e.keyCode === 13 && !disableSearch){
+    if (e.keyCode === 13 && !disableSearch) {
       setSymbolErrors('')
       setSearchWords(prev => prev.replaceAll(/[^A-Za-z0-9\- ]/g, ''))
-      if(path.includes('search')){
+      if (path.includes('search')) {
         let query = new URLSearchParams(loc.search)
         let filtered = []
         let data = {
-          keyword:searchWords,
-          minPrice:query.get('minPrice'),
-          maxPrice:query.get('maxPrice'),
-          ownerId:query.get('ownerId'),
-          order:query.get('order')
+          keyword: searchWords,
+          minPrice: query.get('minPrice'),
+          maxPrice: query.get('maxPrice'),
+          ownerId: query.get('ownerId'),
+          order: query.get('order')
         }
         for (let key in data) {
-            if (data[key]) {
-                filtered.push(`${key}=${data[key]}`)
-            }
+          if (data[key]) {
+            filtered.push(`${key}=${data[key]}`)
+          }
         }
-      let filterstring = filtered.join("&")
-      history.push(`/search?${filterstring}`)
-      }else{
+        let filterstring = filtered.join("&")
+        history.push(`/search?${filterstring}`)
+      } else {
         history.push(`/search?keyword=${searchWords}`)
       }
       e.target.blur()
@@ -94,19 +95,23 @@ const SearchBar = ({searchWords, setSearchWords}) => {
 
   return (
     <div className="search-bar-container">
+      <div className='searchbar-container'>
         <input className='input search-bar'
-                type="text"
-                placeholder='Search for anything'
-                value={searchWords}
-                pattern="[^A-Za-z0-9 ]"
-                title="Only numbers, alphabates and spaces are permitted!"
-                onChange={searchChangeHandler}
-                onKeyDown={handleSearchPress}
-                />
+          type="text"
+          placeholder='Search for anything'
+          value={searchWords}
+          pattern="[^A-Za-z0-9 ]"
+          title="Only numbers, alphabates and spaces are permitted!"
+          onChange={searchChangeHandler}
+          onKeyDown={handleSearchPress}
+        />
+        <div className='magnifying-container' onClick={searchClickHandler}>
+          <img src={Magnifying} disabled={disableSearch} className='input search-submit' ></img>
+        </div>
+      </div>
         {symbolErrors && (
-          <span>{symbolErrors}</span>
+          <span className='search-errors-text'>{symbolErrors}</span>
         )}
-        <button disabled={disableSearch} className='input search-submit' onClick={searchClickHandler}>o-</button>
     </div>
   )
 }
