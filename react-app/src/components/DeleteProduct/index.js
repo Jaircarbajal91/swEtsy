d851cbbb1de17503898a546106b5e7f2fe132795
alice886 <baconhill4tmr@gmail.com>
2022-08-28 01:27:43 -0700

    delete confrimation done

--- react-app/src/components/DeleteProduct/index.js
+++ react-app/src/components/DeleteProduct/index.js
@@ -1,10 +1,11 @@
+import './deleteConfirmation.css'
 
 const DeleteProduct = ({ setShowDelete, setDeleted }) => {
   return (
-    <div>
-      <h1>Are you sure you want to delete this product?</h1>
-      <button onClick={() => setDeleted(true)}>Yes</button>
-      <button onClick={() => setShowDelete(false)}>No</button>
+    <div className='delete-confirmation-modal'>
+      <h1 className='delete-confirmation-question'>Are you sure you want to delete this product?</h1>
+      <button className='delete-confirmation-yes' onClick={() => setDeleted(true)}>Yes</button>
+      <button className='delete-confirmation-no' onClick={() => setShowDelete(false)}>No</button>
     </div>
   )
 }
--- react-app/src/components/DeleteReview/index.js
+++ react-app/src/components/DeleteReview/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { getMyReviewThunk, deleteReviewThunk } from "../../store/review";
+import './deleteConfirmation.css'
 
 const DeleteReview = ({ setDeleteReview, setShowDelete, reviewId }) => {
     const dispatch = useDispatch();
@@ -11,10 +12,10 @@ const DeleteReview = ({ setDeleteReview, setShowDelete, reviewId }) => {
     }
 
     return (
-        <div>
-            <h1>Are you sure you want to delete this Review?</h1>
-            <button onClick={handleDelete}>Yes</button>
-            <button onClick={() => setShowDelete(false)}>No</button>
+        <div className='delete-confirmation-modal'>
+            <h1 className='delete-confirmation-question'>Are you sure you want to delete this Review?</h1>
+            <button className='delete-confirmation-yes' onClick={handleDelete}>Yes</button>
+            <button className='delete-confirmation-no' onClick={() => setShowDelete(false)}>No</button>
         </div>
     )
 }
